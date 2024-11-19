f34460fda6653aaff27121709f83fd6a0a096498
CalebMostyn <mostyncr@miamioh.edu>
2024-11-18 22:37:38 -0500

    Added Skeleton for Data Structure and Scripts

---
+++ scripts/make_training_set.py
@@ -0,0 +1,6 @@
+# This will be the script responsible for randomly sampling the regions for 
+# our training set and pulling in data to the appropriate folders:
+#
+# data/data_point_0
+# data/...
+# data/data_point_n
